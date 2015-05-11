fd2a35d68f0ae8f78aefc43066cb0ce3d451e514
Bruno de Lima e Silva <dlimaun@gmail.com>
2015-05-11 02:10:45 +0000

    Add local storage

--- gct.go
+++ gct.go
@@ -1,29 +1,35 @@
 package main
 
-//import "github.com/google/go-github/github"
 import (
-	"fmt"
-	"os"
-	"net/http"
-	"io/ioutil"
-	"strconv"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 )
 
 type User struct {
-	Login		string
-	Id			int
-	Avatar_url	string
-	Name		string
-	Email		string
+	Login      string
+	Id         int
+	Avatar_url string
+	Name       string
+	Email      string
 }
 
-func main() {
-	userName := os.Args[1]
+const fileName = "users.json"
 
-	user, err := findUser(userName)
-	if err != nil {
+var users []User
+
+func main() {
+	load()
+
+	login := os.Args[1]
+
+	user := findUser(login)
+	if user == nil {
 		fmt.Println("Bad user")
 		return
 	}
@@ -32,13 +38,38 @@ func main() {
 	setGitConfig("user.email", user.Email)
 }
 
-func findUser(userName string) (user *User, err error){
-	request := "https://api.github.com/users/"+userName
-	fmt.Println("Request "+ request)
+func load() {
+	file, _ := os.Open(fileName)
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	decoder.Decode(&users)
+}
+
+func findUser(login string) *User {
+	user := lUser(login)
+	if user == nil {
+		fmt.Println("Can't find user on local data.")
+		user = dUser(login)
+	}
+	return user
+}
+
+func lUser(login string) *User {
+	for i := 0; i < len(users); i++ {
+		if strings.EqualFold(login, users[i].Login) {
+			return &users[i]
+		}
+	}
+	return nil
+}
+
+func dUser(login string) (user *User) {
+	request := "https://api.github.com/users/" + login
+	fmt.Println("Request " + request)
 	resp, err := http.Get(request)
 	if err != nil {
 		fmt.Println("Can't request the user. Maybe without internet.")
-		return nil, err
+		return nil
 	}
 
 	// calls 'resp.Body.Close()' after the ending of this funcion
@@ -49,12 +80,22 @@ func findUser(userName string) (user *User, err error){
 	err = json.Unmarshal(body, &user)
 	if err != nil {
 		fmt.Println("Can't read the json")
-		return nil, err
+		return nil
 	}
-	return user, nil
+
+	go save(user)
+	return user
 }
 
-func setGitConfig(config string, value string){
+func save(user *User) {
+	users = append(users, *user)
+	file, _ := os.Create(fileName)
+	defer file.Close()
+	enc := json.NewEncoder(file)
+	enc.Encode(&users)
+}
+
+func setGitConfig(config string, value string) {
 	if len(value) > 0 {
 		fmt.Println(value)
 		err := exec.Command("git", "config", config, strconv.Quote(value)).Run()
@@ -67,4 +108,3 @@ func setGitConfig(config string, value string){
 		fmt.Printf("Can't set %s with a empty value.\n", config)
 	}
 }
-
